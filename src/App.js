bca6c3b5936d33dbf2d589c9645c838e55431349
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2022-03-13 23:20:37 -0500

    Created the Weather Icon files and components to display temperature in different units

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by
-        <a
-          href="https//www.linkedin.com/in/tamirha/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Tamirha Giraldo
-        </a>
-        {""} and is {""}
-        <a
-          href="https://github.com/TamirhaG/my-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.linkedin.com/in/tamirha/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tamirha Giraldo
+          </a>
+          {""} and is {""}
+          <a
+            href="https://github.com/TamirhaG/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
