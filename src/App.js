0a7bcfe3ba0ee1d7f0739bcc80a69a0a3db4618e
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2022-03-13 22:56:21 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a
+          href="https//www.linkedin.com/in/tamirha/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Tamirha Giraldo
+        </a>
+        {""} and is {""}
+        <a
+          href="https://github.com/TamirhaG/my-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
